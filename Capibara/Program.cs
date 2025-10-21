26253f50f0be8dea825080a259fe61c8e8eee7ae
Paquito86 <66384054+Paquito86@users.noreply.github.com>
2025-10-21 11:38:24 +0200

    Update awake endpoint response message

--- Capibara/Program.cs
+++ Capibara/Program.cs
@@ -142,7 +142,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 // Public awake endpoint
-app.MapGet("/awake/", () => Results.Text("I'm sleeping!", "text/plain"))
+app.MapGet("/awake/", () => Results.Text("You woke me up! What the hell do you want, huh?", "text/plain"))
    .AllowAnonymous()
    .WithOpenApi(op =>
    {
